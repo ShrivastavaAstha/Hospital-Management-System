f7481703075921afc374c901d326f8729ce3cd7d
Astha <astha2333@gmail.com>
2025-07-25 11:19:03 +0530

    modified

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// const cors = require("cors");
-// const path = require("path");
+const cors = require("cors");
+const path = require("path");
 
-// require("dotenv").config();
+require("dotenv").config();
 
 const app = express();
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // app.use("/uploads", express.static("uploads"));
-// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // // Routes
 // const patientRoute = require("./routes/patientRoute");
 // app.use("/api/patients", patientRoute);
