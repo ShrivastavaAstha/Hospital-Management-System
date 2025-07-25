4b99410788ca6ed4360348c5b928adf880d42453
Astha <astha2333@gmail.com>
2025-07-25 10:46:11 +0530

    modified

--- backend/routes/doctorRoutes.js
+++ backend/routes/doctorRoutes.js
@@ -46,14 +46,14 @@ router.put(
 );
 
 // Get user (doctor) by ID
-router.get("/:id", async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) return res.status(404).json({ error: "User not found" });
-    res.json(user);
-  } catch (err) {
-    res.status(500).json({ error: "Server error" });
-  }
-});
+// router.get("/:id", async (req, res) => {
+//   try {
+//     const user = await User.findById(req.params.id);
+//     if (!user) return res.status(404).json({ error: "User not found" });
+//     res.json(user);
+//   } catch (err) {
+//     res.status(500).json({ error: "Server error" });
+//   }
+// });
 
 module.exports = router;
